d08e0ecfee61ac51b4360d860cab2528f2a595f9
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2020-12-07 16:52:25 +0100

    added a pagination component

--- src/Context.jsx
+++ src/Context.jsx
@@ -7,15 +7,30 @@ const client = createClient(
   "563492ad6f91700001000001068dd1d344c748c18d09a44c6b2b840e"
 );
 const ContextProvider = ({ children }) => {
-  const [config, setConfig] = useState({ query: "Nature", per_page: 40 });
+  const [config, setConfig] = useState({
+    query: "Nature",
+    per_page: 40,
+    page: 1,
+  });
 
   const [allPhotos, setAllPhotos] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [favoritePhotos, setFavoritePhotos] = useState([]);
+  const [paginationReleted, setPaginationReleted] = useState({
+    perPage: 40,
+    page: 1,
+    totalResults: 10,
+  });
 
   useEffect(() => {
     client.photos.search(config).then((res) => {
       setAllPhotos(res.photos);
+      const { page, per_page, total_results } = res;
+      setPaginationReleted({
+        currentPage: page,
+        perPage: per_page,
+        totalResults: total_results,
+      });
     });
   }, [config]);
 
@@ -50,7 +65,9 @@ const ContextProvider = ({ children }) => {
         favoritePhotos,
         addToFavorite,
         removeFromFav,
-        setConfig,
+        requestConfig: config,
+        setRequestConfig: setConfig,
+        paginationReleted,
       }}
     >
       {children}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import React, { useContext, useState } from "react";
 import Photos from "../components/Photos";
 import { Context } from "../Context";
+import Pagination from "../components/Pagination";
 
 function Home() {
   const [userInput, setUserInput] = useState("");
   const [userInputNumber, setUserInputNumber] = useState("");
 
-  const { allPhotos, setConfig } = useContext(Context);
-  console.log(userInput);
+  const { allPhotos, requestConfig, setRequestConfig } = useContext(Context);
 
+  function changePage(action) {
+    const newPage =
+      action === "next" ? requestConfig.page + 1 : requestConfig.page - 1;
+    setRequestConfig({ ...requestConfig, page: newPage });
+  }
   return (
     <main className="home-container">
       <div>
@@ -24,15 +29,21 @@ function Home() {
           onChange={(e) => setUserInputNumber(e.currentTarget.value)}
         />
         <button
+          className="button-hover"
           disabled={!userInput.length}
           onClick={() =>
-            setConfig({ query: userInput, per_page: userInputNumber })
+            setRequestConfig({
+              page: 1,
+              query: userInput,
+              per_page: userInputNumber,
+            })
           }
         >
           Search
         </button>
       </div>
       <Photos photos={allPhotos} />
+      {allPhotos.length !== 0 && <Pagination changePage={changePage} />}
     </main>
   );
 }
